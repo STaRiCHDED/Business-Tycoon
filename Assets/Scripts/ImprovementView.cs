a2e2d53d2d183bb7a57955ad1d951ae755e4a3b9
byseinbolt <busovitor@gmail.com>
2023-03-23 15:41:29 +0300

    add ImprovementView.cs

---
+++ Assets/Scripts/ImprovementView.cs
@@ -0,0 +1,24 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class ImprovementView : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI _nameLabel;
+    
+    [SerializeField]
+    private TextMeshProUGUI _additionalPercentageIncomeLabel;
+    
+    [SerializeField]
+    private TextMeshPro _stateLabel;
+
+    private string _purchased = "Purchased";
+
+    public void Show(Improvement improvement)
+    {
+        _nameLabel.text = improvement.Name;
+        _additionalPercentageIncomeLabel.text = "Income +" + improvement.AdditionalPercentageIncome + "%";
+        _stateLabel.text = "Price" + improvement.Price;
+    }
+
+}
