3fa9efecf5d4b744b871bc3db75ed43028a66d6d
byseinbolt <busovitor@gmail.com>
2023-03-23 21:44:31 +0300

    refactor ImprovementView.cs

--- Assets/Scripts/ImprovementView.cs
+++ Assets/Scripts/ImprovementView.cs
@@ -14,11 +14,10 @@ public class ImprovementView : MonoBehaviour
 
     private string _purchased = "Purchased";
 
-    public void Show(Improvement improvement)
+    public void Show(ImprovementModel improvement)
     {
         _nameLabel.text = improvement.Name;
         _additionalPercentageIncomeLabel.text = "Income +" + improvement.AdditionalPercentageIncome + "%";
         _stateLabel.text = "Price" + improvement.Price;
     }
-
 }
