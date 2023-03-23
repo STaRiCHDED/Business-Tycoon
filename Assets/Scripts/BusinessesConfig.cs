31292dec8034be989a046635c0b8f0114ea0783a
byseinbolt <busovitor@gmail.com>
2023-03-23 21:42:20 +0300

    add BusinessesConfig.cs

---
+++ Assets/Scripts/BusinessesConfig.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Businesses", menuName = "Businesses")]
+public class BusinessesConfig : ScriptableObject
+{
+    public IReadOnlyList<BusinessModel> Businesses => _businesses;
+    
+    [SerializeField]
+    private List<BusinessModel> _businesses;
+}
