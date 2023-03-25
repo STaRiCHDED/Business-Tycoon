74859f4f071feb646ec75e191776bd7db58e1711
byseinbolt <busovitor@gmail.com>
2023-03-25 23:03:28 +0300

    refactor: change BusinessData.cs

--- Assets/Scripts/ConfigDataProvider/BusinessData.cs
+++ Assets/Scripts/ConfigDataProvider/BusinessData.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Newtonsoft.Json;
 
 public class BusinessData
@@ -9,5 +10,5 @@ public class BusinessData
     [JsonProperty("balance")] 
     public int Balance { get; set;}
     [JsonProperty("businesses")] 
-    public Business[] Businesses { get; set;}
+    public List<Business> Businesses { get; set;}
 }
