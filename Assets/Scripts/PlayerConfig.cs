fcc5d432e5d83b7915b6ea2675ee059952bcb49c
byseinbolt <busovitor@gmail.com>
2023-03-25 16:20:58 +0300

    feat: PlayerConfig.cs and PlayerBalanceConfig.asset

---
+++ Assets/Scripts/PlayerConfig.cs
@@ -0,0 +1,12 @@
+﻿using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu(fileName = "PlayerBalanceConfig", menuName = "PlayerBalanceConfig")]
+public class PlayerConfig : ScriptableObject
+{
+    public PlayerBalanceModel PlayerBalanceModel => _playerBalanceModel;
+    
+    [SerializeField]
+    private PlayerBalanceModel _playerBalanceModel;
+}
