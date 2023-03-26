a1e4bce4e860235a28d1ee0d907ce11ced1ec11a
byseinbolt <busovitor@gmail.com>
2023-03-26 12:45:19 +0300

    feat: add IConfigService.cs

---
+++ Assets/Scripts/Services/IConfigService.cs
@@ -0,0 +1,8 @@
+﻿namespace Services
+{
+    public interface IConfigService : IService
+    {
+        int RecalculateUpgradePrice(int level, int basePrice);
+        int RecalculateIncome(int level, int baseIncome, int firstImprovementBoost, int secondImprovementBoost);
+    }
+}
