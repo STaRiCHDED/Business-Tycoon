02ce7369512d898143241001e350993b29fa8a87
Golipeg <golipeg@gmail.com>
2023-03-26 16:50:13 +0300

    Added ImprovementController.cs

--- Assets/Scripts/Controllers/ImprovementController.cs
+++ Assets/Scripts/Controllers/ImprovementController.cs
@@ -1,9 +1,36 @@
+using System;
+using Models;
+using Services;
 using UnityEngine;
+using Views;
 
 namespace Controllers
 {
     public class ImprovementController : MonoBehaviour
     {
-    
+        private ImprovementView _improvementView;
+        private ImprovementModel _improvementModel;
+        
+
+        private void Initialize(ImprovementConfigModel improvementConfigModel )
+        {
+            _improvementView = GetComponent<ImprovementView>();
+            _improvementModel = new ImprovementModel(improvementConfigModel);
+            _improvementView.SetClickCallBack(BuyImprovement);
+
+        }
+
+        public void BuyImprovement()
+        {
+            var improvementPrice = _improvementModel.Price;
+           var balanceService= ServiceLocator.Instance.GetSingle<IBalanceService>();
+           if (balanceService.HasEnoughMoney(improvementPrice))
+           {
+               _improvementModel.ChangeState(true);
+               balanceService.Pay(improvementPrice);
+               _improvementView.Show(_improvementModel);
+           }
+           
+        }
     }
 }
