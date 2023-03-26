33fb5f3a290abc99618f0ddc938fca1562371ff2
byseinbolt <busovitor@gmail.com>
2023-03-26 14:00:43 +0300

    feat: add ConfigService.cs

---
+++ Assets/Scripts/Services/ConfigService.cs
@@ -0,0 +1,15 @@
+﻿namespace Services
+{
+    public class ConfigService : IConfigService
+    {
+        public int RecalculateUpgradePrice(int level, int basePrice)
+        {
+            return (level + 1) * basePrice;
+        }
+
+        public int RecalculateIncome(int level, int baseIncome, int firstImprovementBoost, int secondImprovementBoost)
+        {
+            return level * baseIncome * (1 + firstImprovementBoost + secondImprovementBoost);
+        }
+    }
+}
