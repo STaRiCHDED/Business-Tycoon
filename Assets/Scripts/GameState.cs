d1647677d8760f8237bb7d9924e2d11788679484
byseinbolt <busovitor@gmail.com>
2023-04-02 13:44:35 +0300

    feat: add GameState.cs

---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,24 @@
+using IngameStateMachine;
+
+public class GameState : IState
+{
+    public void Dispose()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Initialize(StateMachine stateMachine)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnEnter()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnExit()
+    {
+        throw new System.NotImplementedException();
+    }
+}
