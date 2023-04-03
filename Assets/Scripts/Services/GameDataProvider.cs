f6c81a81f779383f53f080091f09d36efe294267
byseinbolt <busovitor@gmail.com>
2023-04-03 11:04:20 +0300

    feat: add GameDataProvider.cs

---
+++ Assets/Scripts/Services/GameDataProvider.cs
@@ -0,0 +1,23 @@
+﻿using Models;
+
+namespace Services
+{
+    public class GameDataProvider : IGameDataProvider
+    {
+        private readonly IConfigService _configService;
+        private readonly IBalanceService _balanceService;
+
+        public GameDataProvider(IConfigService configService, IBalanceService balanceService)
+        {
+            _configService = configService;
+            _balanceService = balanceService;
+        }
+        public SaveDataModel GetCurrentData()
+        {
+            var businessModels = _configService.BusinessModels;
+            var playerBalanceModel = _balanceService.PlayerBalanceModel;
+            var saveData = new SaveDataModel(playerBalanceModel, businessModels);
+            return saveData;
+        }
+    }
+}
---
+++ Assets/Scripts/Services/IGameDataProvider.cs
@@ -0,0 +1,9 @@
+﻿using Models;
+
+namespace Services
+{
+    public interface IGameDataProvider : IService
+    {
+        public SaveDataModel GetCurrentData();
+    }
+}
