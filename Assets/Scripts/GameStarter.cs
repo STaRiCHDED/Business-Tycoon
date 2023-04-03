8e2c4ff08233a664ea758a353f27d35f1cb619a9
byseinbolt <busovitor@gmail.com>
2023-04-03 11:06:44 +0300

    refactor: initialize stateMachine

--- Assets/Scripts/GameStarter.cs
+++ Assets/Scripts/GameStarter.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Events;
 using IngameStateMachine;
 using Models;
 using Services;
+using SimpleEventBus.Disposables;
+using UnityEditor;
 using UnityEngine;
 using Views;
 
@@ -9,34 +11,39 @@ public class GameStarter : MonoBehaviour
 {
     [SerializeField]
     private GameScreen _gameScreen;
-    
+
     [SerializeField]
     private StartScreen _startScreen;
-    
+
     [SerializeField]
     private BusinessesSpawner _businessesSpawner;
-    
+
     private StateMachine _stateMachine;
+
     private void Awake()
     {
         var initializationState = new InitializationState();
-        var serviceLocator = initializationState.RegisterServices();
+        initializationState.RegisterServices();
 
-        var fileService = serviceLocator.GetSingle<IFileService>();
-        
-        var saveDataModel = fileService.Load();
-        serviceLocator.RegisterSingle<IBalanceService>(new BalanceService(saveDataModel.PlayerBalanceModel));
-        
-        
-        var startState = new StartState(_startScreen,saveDataModel);
-        var gameState = new GameState(_businessesSpawner, saveDataModel,_gameScreen);
-        _stateMachine = new StateMachine(initializationState,startState, gameState);
+        var startState = new StartState(_startScreen);
+        var gameState = new GameState(_gameScreen,_businessesSpawner);
+
+        _stateMachine = new StateMachine(initializationState, startState, gameState);
         _stateMachine.Initialize();
         _stateMachine.Enter<InitializationState>();
     }
-
+    
+    private void SaveGame()
+    {
+        var fileService = ServiceLocator.Instance.GetSingle<IFileService>();
+        var gameDataProvider = ServiceLocator.Instance.GetSingle<IGameDataProvider>();
+        var saveData = gameDataProvider.GetCurrentData();
+        fileService.Save(saveData);
+    }
+    
     private void OnDestroy()
     {
         ServiceLocator.Instance.Dispose();
+        SaveGame();
     }
 }
